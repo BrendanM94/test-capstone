170aa6017ac90d43aeb137c11f495e9497170878
Brendan <brendanmadden94@hotmail.com>
2025-12-01 16:22:05 +0000

    set up project & add. added details to make runserver show html

---
+++ todo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Todo
+
+
+def todo_list(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description", "")
+
+        # super simple validation: only create if there's a title
+        if title:
+            Todo.objects.create(title=title, description=description)
+
+        # redirect so refresh doesn't re-submit the form
+        return redirect("todo_list")
+
+    # GET request: just show the list
+    todos = Todo.objects.all().order_by("-created_at")
+    return render(request, "todo/todo_list.html", {"todos": todos})
