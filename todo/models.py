5c3924a4d2b2ca90b80972dfefc2dd93a7ed5dcd
Brendan <brendanmadden94@hotmail.com>
2025-12-04 16:38:24 +0000

    updated readme

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User   # ⬅ add this
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  # ⬅ add this line
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
